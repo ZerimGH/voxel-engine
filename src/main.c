8567c4a32e9b98e7526a1808f3104e1d606ee9df
lewis <lewdon0001@gmail.com>
2025-10-09 23:00:49 +0100

    window initialisation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "nuGL.h"
 
 int main(void) {
-  printf("hlelo, world!\n");
+  nu_Window *window = nu_create_window(600, 600, NULL, NULL);
+  while(!glfwWindowShouldClose(window->glfw_window)) {
+    // Update input
+    nu_update_input(window);
+    // Rendering 
+    nu_start_frame(window);
+    nu_end_frame(window);
+  }
+  nu_destroy_window(&window);
   return 0;
 }
