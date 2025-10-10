8cbc04efbcb1ca9737bb94c6cfeda9f53c566f87
lewis <lewdon0001@gmail.com>
2025-10-10 02:49:02 +0100

    cleaned up a bit

--- src/chunk.c
+++ src/chunk.c
@@ -1,5 +1,7 @@
 #include "chunk.h"
 
+#define GREEDY
+
 Chunk *create_chunk(int chunk_x, int chunk_y, int chunk_z) {
   Chunk *chunk = calloc(1, sizeof(Chunk));
   if (!chunk) {
@@ -39,10 +41,12 @@ void generate_chunk(Chunk *chunk) {
   int ccy = chunk->coords[1] * CHUNK_HEIGHT;
   int ccz = chunk->coords[2] * CHUNK_LENGTH;
   for (size_t x = 0; x < CHUNK_WIDTH; x++) {
-    int terrain_height = rand() % 10;
-    for (size_t y = 0; y < CHUNK_HEIGHT; y++) {
-      int gy = ccy + (int)y;
-      for (size_t z = 0; z < CHUNK_LENGTH; z++) {
+    int gx = ccx + x;
+    for (size_t z = 0; z < CHUNK_LENGTH; z++) {
+      int gz = ccz + z;
+      int terrain_height = octave_noise_2d(gx, gz, 3, 0.5, 1.6, 128, 1) * 50 + 50;
+      for (size_t y = 0; y < CHUNK_HEIGHT; y++) {
+        int gy = ccy + y;
         BlockType block = gy <= terrain_height ? BlockSolid : BlockAir;
         size_t idx = CHUNK_INDEX(x, y, z);
         chunk->blocks[idx] = (Block){.type = block};
@@ -56,6 +60,8 @@ typedef struct {
   GLfloat pos[3];
   GLfloat tex[2];
 } BlockVertex;
+
+#ifdef GREEDY
 
 static const int axis_uv[3][2] = {{1, 2}, {0, 2}, {0, 1}};
 static const int dims[3] = {CHUNK_WIDTH, CHUNK_HEIGHT, CHUNK_LENGTH};
@@ -294,3 +300,129 @@ void mesh_chunk(Chunk *chunk) {
   free(verts);
   // free(tverts);
 }
+#else
+
+typedef struct {
+  GLfloat pos[3];
+  GLfloat tex[2];
+} Vertex;
+
+size_t vertex_num = 2;
+size_t vertex_sizes[] = {sizeof(GLfloat), sizeof(GLfloat)};
+size_t vertex_counts[] = {3, 2};
+GLenum vertex_types[] = {GL_FLOAT, GL_FLOAT};
+
+Vertex cube[] = {
+    // Front face
+    {{0, 0, 1}, {0, 0}},
+    {{1, 0, 1}, {1, 0}},
+    {{1, 1, 1}, {1, 1}},
+    {{0, 0, 1}, {0, 0}},
+    {{1, 1, 1}, {1, 1}},
+    {{0, 1, 1}, {0, 1}},
+
+    // Back face
+    {{1, 0, 0}, {0, 0}},
+    {{0, 0, 0}, {1, 0}},
+    {{0, 1, 0}, {1, 1}},
+    {{1, 0, 0}, {0, 0}},
+    {{0, 1, 0}, {1, 1}},
+    {{1, 1, 0}, {0, 1}},
+
+    // Left face
+    {{0, 0, 0}, {0, 0}},
+    {{0, 0, 1}, {1, 0}},
+    {{0, 1, 1}, {1, 1}},
+    {{0, 0, 0}, {0, 0}},
+    {{0, 1, 1}, {1, 1}},
+    {{0, 1, 0}, {0, 1}},
+
+    // Right face
+    {{1, 0, 1}, {0, 0}},
+    {{1, 0, 0}, {1, 0}},
+    {{1, 1, 0}, {1, 1}},
+    {{1, 0, 1}, {0, 0}},
+    {{1, 1, 0}, {1, 1}},
+    {{1, 1, 1}, {0, 1}},
+
+    // Top face
+    {{0, 1, 1}, {0, 0}},
+    {{1, 1, 1}, {1, 0}},
+    {{1, 1, 0}, {1, 1}},
+    {{0, 1, 1}, {0, 0}},
+    {{1, 1, 0}, {1, 1}},
+    {{0, 1, 0}, {0, 1}},
+
+    // Bottom face
+    {{0, 0, 0}, {0, 0}},
+    {{1, 0, 0}, {1, 0}},
+    {{1, 0, 1}, {1, 1}},
+    {{0, 0, 0}, {0, 0}},
+    {{1, 0, 1}, {1, 1}},
+    {{0, 0, 1}, {0, 1}},
+};
+
+static inline bool is_solid(BlockType t) {
+  return t != BlockAir;
+}
+
+static void chunk_add_cube(Chunk *chunk, BlockType neighbours[6], size_t x, size_t y, size_t z) {
+  if (!chunk || !chunk->mesh) return;
+
+  int ccx = chunk->coords[0] * CHUNK_WIDTH;
+  int ccy = chunk->coords[1] * CHUNK_HEIGHT;
+  int ccz = chunk->coords[2] * CHUNK_LENGTH;
+
+  for (size_t face = 0; face < 6; face++) {
+    if (neighbours[face] != BlockAir) continue;
+
+    Vertex face_vertices[6];
+    memcpy(face_vertices, &cube[face * 6], sizeof(face_vertices));
+
+    for (size_t i = 0; i < 6; i++) {
+      face_vertices[i].pos[0] += ccx + x;
+      face_vertices[i].pos[1] += ccy + y;
+      face_vertices[i].pos[2] += ccz + z;
+    }
+
+    nu_mesh_add_bytes(chunk->mesh, sizeof(face_vertices), face_vertices);
+  }
+}
+
+void get_neighbours(Chunk *chunk, BlockType neighbours[6], size_t x, size_t y, size_t z) {
+  if (!chunk) return;
+
+  neighbours[0] = (z == CHUNK_LENGTH - 1) ? BlockAir : chunk->blocks[CHUNK_INDEX(x, y, z + 1)].type;
+  neighbours[1] = (z == 0) ? BlockAir : chunk->blocks[CHUNK_INDEX(x, y, z - 1)].type;
+  neighbours[2] = (x == 0) ? BlockAir : chunk->blocks[CHUNK_INDEX(x - 1, y, z)].type;
+  neighbours[3] = (x == CHUNK_WIDTH - 1) ? BlockAir : chunk->blocks[CHUNK_INDEX(x + 1, y, z)].type;
+  neighbours[4] = (y == CHUNK_HEIGHT - 1) ? BlockAir : chunk->blocks[CHUNK_INDEX(x, y + 1, z)].type;
+  neighbours[5] = (y == 0) ? BlockAir : chunk->blocks[CHUNK_INDEX(x, y - 1, z)].type;
+}
+
+void mesh_chunk(Chunk *chunk) {
+  if (!chunk || !chunk->generated || !chunk->blocks) return;
+
+  if (chunk->mesh) nu_destroy_mesh(&chunk->mesh);
+  chunk->mesh = nu_create_mesh(vertex_num, vertex_sizes, vertex_counts, vertex_types);
+
+  for (size_t x = 0; x < CHUNK_WIDTH; x++) {
+    for (size_t y = 0; y < CHUNK_HEIGHT; y++) {
+      for (size_t z = 0; z < CHUNK_LENGTH; z++) {
+        size_t idx = CHUNK_INDEX(x, y, z);
+        BlockType block = chunk->blocks[idx].type;
+
+        if (is_solid(block)) {
+          BlockType neighbours[6] = {BlockAir};
+          get_neighbours(chunk, neighbours, x, y, z);
+          chunk_add_cube(chunk, neighbours, x, y, z);
+        }
+      }
+    }
+  }
+
+  nu_send_mesh(chunk->mesh);
+  nu_free_mesh(chunk->mesh);
+}
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <assert.h>
+#include <limits.h>
 #define NUGL_DEBUG
 #include "camera.h"
 #include "chunk.h"
@@ -9,19 +11,30 @@
 #define MOVE_SPEED 0.3f
 
 int main(void) {
+  assert((long)CHUNK_WIDTH * (long)CHUNK_HEIGHT *(long)CHUNK_LENGTH < INT_MAX / 100); // Make sure chunk size doesnt overflow anywhere
+
+  // Create the window
   nu_Window *window = nu_create_window(600, 600, NULL, NULL);
   glfwSetInputMode(window->glfw_window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  // Create shader program
   nu_Program *program = nu_create_program(2, "shaders/block.vert", "shaders/block.frag");
   nu_register_uniform(program, "uMVP", GL_FLOAT_MAT4);
+  // Create, generate and mesh a single chunk
   Chunk *chunk = create_chunk(0, 0, 0);
+START_TIMER(generate_chunk);
   generate_chunk(chunk);
-  START_TIMER(mesh_chunk);
+END_TIMER(generate_chunk);
+START_TIMER(mesh_chunk);
   mesh_chunk(chunk);
-  END_TIMER(mesh_chunk);
+END_TIMER(mesh_chunk);
+
+  // Debug prints
+  nu_print_window(window);
+  nu_print_program(program);
   nu_print_mesh(chunk->mesh);
 
   // Create a camera
-  Camera *cam = create_camera(0.1, 200, 90.f);
+  Camera *cam = create_camera(0.1, 512, 90.f);
 
   while (!glfwWindowShouldClose(window->glfw_window)) {
     // Update input
@@ -32,7 +45,7 @@ int main(void) {
     if (nu_get_key_state(window, GLFW_KEY_SPACE)) camera_move(cam, (vec3){0, MOVE_SPEED, 0});
     if (nu_get_key_state(window, GLFW_KEY_LEFT_SHIFT)) camera_move(cam, (vec3){0, -MOVE_SPEED, 0});
     camera_rotate(cam, nu_get_delta_mouse_x(window) * SENS, -nu_get_delta_mouse_y(window) * SENS);
-    nu_update_input(window);
+    nu_update_input(window); // Idk why i have to do this after using input?
 
     // Send uniforms
     mat4 vp;
@@ -48,9 +61,15 @@ int main(void) {
     nu_render_mesh(chunk->mesh);
     nu_end_frame(window);
   }
+  printf("Exiting...\n");
+
+  // Cleanup
   nu_destroy_window(&window);
   nu_destroy_program(&program);
   destroy_camera(&cam);
   destroy_chunk(&chunk);
+
+  nu_print_window(window);
+  nu_print_program(program);
   return 0;
 }
